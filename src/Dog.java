b4e0123404371871f88a66cdd401c37f9e330069
Jordan Litsas <jordanlitsas@live.com.au>
2025-06-01 21:39:17 +1000

    dog

---
+++ src/Dog.java
@@ -0,0 +1,10 @@
+public class Dog {
+    public String action;
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+    public String getAction() {
+        return this.action;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        Dog dog = new Dog();
+        dog.setAction("BARK");
+        System.out.println(dog.getAction());
+        for (int i = 0; i < 3; i++) {
+            System.out.println(dog.getAction());
+        }
     }
 }
